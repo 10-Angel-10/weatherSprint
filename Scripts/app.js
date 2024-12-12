e9958d2a074f6704c69467bbbfc277a6df264ad8
10-Angel-10 <aocuevas123@gmail.com>
2024-12-11 16:37:22 -0800

    declared the all of my ids

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,37 @@
 import { APIKEY } from './environment.js';   
 
 
+
+
+let cityName = document.getElementById("cityName");
+let todaysTmp = document.getElementById("todaysTmp");
+let weekTime = document.getElementById("weekTime");
+let Month = document.getElementById("Month");
+let maxMinTmp = document.getElementById("maxMinTmp");
+let type1Icon = document.getElementById("type1Icon");
+let type2Icon = document.getElementById("type2Icon");
+let type3Icon = document.getElementById("type3Icon");
+let type1IconText = document.getElementById("type1IconText");
+let type2IconText = document.getElementById("type2IconText");
+let type3IconText = document.getElementById("type3IconText");
+let nextDay1Text = document.getElementById("nextDay1Text");
+let nextDay2Text = document.getElementById("nextDay2Text");
+let nextDay3Text = document.getElementById("nextDay3Text");
+let nextDay4Text = document.getElementById("nextDay4Text");
+let nextDay5Text = document.getElementById("nextDay5Text");
+let nextDay1Icon = document.getElementById("nextDay1Icon");
+let nextDay2Icon = document.getElementById("nextDay2Icon");
+let nextDay3Icon = document.getElementById("nextDay3Icon");
+let nextDay4Icon = document.getElementById("nextDay4Icon");
+let nextDay5Icon = document.getElementById("nextDay5Icon");
+let nextDay1Tmp = document.getElementById("nextDay1Tmp");
+let nextDay2Tmp = document.getElementById("nextDay2Tmp");
+let nextDay3Tmp = document.getElementById("nextDay3Tmp");
+let nextDay4Tmp = document.getElementById("nextDay4Tmp");
+let nextDay5Tmp = document.getElementById("nextDay5Tmp");
+
+
+
 //Geo location is a built in API that allows the user to share there location apon request.
 
 //navigator.geolocation this return geolocation object
